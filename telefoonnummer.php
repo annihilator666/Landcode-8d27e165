5135b957af92b8704052d3c27e53a4bdb3abe22d
Steven van Zelst <STUDENTNUMMER@talnet.nl>
2020-07-25 16:55:02 +0200

    Uitwerking van de exercise

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -2,9 +2,8 @@
 
 $re = '/\A([\+]?)(\d{2})(\d{8,9})\z/';
 if (preg_match($re, $argv[1], $matches, PREG_OFFSET_CAPTURE)) {
-
     if ($matches[1][0] === '+') {
-        echo $matches[1][0].$matches[2][0] . PHP_EOL;
+        echo $matches[1][0] . $matches[2][0] . PHP_EOL;
     }
     print_r("it's a match!") . PHP_EOL;
 } else {
